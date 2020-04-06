1066265aecc678332546cd0de0667c0befe4160d
oussema metoui <oussemametoui94@gmail.com>
2020-04-06 21:55:55 +0100

    add credentials params

--- credentials.js
+++ credentials.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 module.exports = {
   key: fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/cert.pem', 'utf8')
+  cert: fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/cert.pem', 'utf8'),
+  rejectUnauthorized: false,
+  requestCert: true,
+  agent: false,
+  strictSSL: false
+
 };
