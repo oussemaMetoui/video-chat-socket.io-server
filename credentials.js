4f3bce4edb7a6e8d41c7999865a2bf3e258bd913
oussema metoui <oussemametoui94@gmail.com>
2020-04-06 21:41:43 +0100

    change port

--- credentials.js
+++ credentials.js
@@ -1,7 +1,5 @@
-// const fs = require('fs');
+const fs = require('fs');
 module.exports = {
-  key: null,
-  cert: null
-  // key: fs.readFileSync('privkey.pem', 'utf8'),
-  // cert: fs.readFileSync('fullchain.pem', 'utf8')
+  key: fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/privkey.pem', 'utf8'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sprint-app.com/cert.pem', 'utf8')
 };
